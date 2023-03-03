b1ac50bde03dc37d2ce0f959e06ff46a459cc902
Foster Ellis <fellis@internal.premiernetworks.us>
2023-03-02 19:20:04 -0600

    added an extra printline to satisfy requirements

--- src/Greeter.java
+++ src/Greeter.java
@@ -4,6 +4,7 @@ public class Greeter {
 	public void greet() {
 		System.out.println("Hello!");
 		System.out.println("this mfer took an office hours visit to get working and it aint even the PROJECT project");
+		System.out.println("i have pulled"); //printline to console
 	}
 
 }
